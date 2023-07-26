39e2cce60f67f54543ea21d73777eb641c240835
kys0411 <32180846@dankook.ac.kr>
2023-07-26 20:48:48 +0900

    main메서드 내용 삭제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,6 @@
-import util.Order;
-import util.SelectBuilder;
-import util.Where;
-import util.statement.From;
-import util.statement.Select;
-
-import static util.constant.OrderType.DESC;
-import static util.constant.Table.PERSON;
 
 public class Main {
     public static void main(String[] args) {
-        Select select = new Select("*");
 
-        From from = new From(PERSON);
-
-        Where where = new Where.Builder()
-                .greater("age")
-                .and()
-                .equals("name")
-                .build();
-
-        Order order = new Order.Builder()
-                .query("id", DESC)
-                .build();
-
-        String sql = new SelectBuilder()
-                .select(select)
-                .from(from)
-                .where(where)
-                .orderBy(order)
-                .build();
-
-        System.out.println(sql);
     }
 }
