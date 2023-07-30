65d559abd6a504c7507f9c264aabdc5b22da8814
kys0411 <32180846@dankook.ac.kr>
2023-07-30 16:16:32 +0900

    person 도메인 추가

---
+++ src/main/java/domain/Person.java
@@ -0,0 +1,18 @@
+package domain;
+
+public class Person {
+
+    private Long id;
+    private String name;
+    private int age;
+    private String address;
+
+    public Person(Long id, String name, int age, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+
+}
